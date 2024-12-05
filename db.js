82dccba0206466c10ac80de8ce9beafb5f6f6fdb
yigtuyumz <yigtuyumz@gmail.com>
2024-12-06 01:42:51 +0300

    improved connection safety and debug information

--- db.js
+++ db.js
@@ -1,88 +1,94 @@
-const mysql = require('mysql'); 
+/* if you install mysql as a global package, consider to change path. */
+const mysql = require('mysql');
 
 const __DEBUG__ = 1;
 
 class DbConnection {
-    // mysql secenekleri, bir obje olarak disaridan aliniyor.
-    constructor(options) {
-        this.options = options;
-    }
+  constructor(options) {
+    this.options = options;
+    this.connection = null;
+  }
 
-    connect(host, user, password, database = null, port = 3306) {
-        this.connection = mysql.createConnection(Object.assign({}, this.options, {
-            host: host,
-            user: user,
-            password: password,
-            database: database,		// opsiyonel
-            port: port,				// opsiyonel, varsayilan 3306
-        }));
+  connect(host, user, password, database = null, port = 3306) {
+    this.connection = mysql.createConnection(Object.assign({},
+      this.options,
+      {
+        host: host,
+        user: user,
+        password: password,
+        database: database,
+        port: port,
+      }
+    ));
+    return (new Promise((resolve, reject) => {
+      this.connection.connect((error) => {
+        if (error) {
+          this.connection = null;
+          reject(error);
 
-        return (new Promise((resolve, reject) => {
-            this.connection.connect((error) => {
-                if (error) {
-                    reject();
-                    throw (error);
-                } else {
-                    if (__DEBUG__) {
-                        console.log('MySQL baglantisi basarili... thread : ' + this.connection.threadId);
-                    }
-                    resolve();
-                }
-            });
-        }));
-    }
-
-    async send_query(sqlStmt, values = []) {
-        if (!this.connect) {
-            throw ('Veritabanina baglanilamadi.');
+        } else {
+          if (__DEBUG__) {
+            console.log("MySQL Connection successful. thread: "
+              + this.connection.threadId);
+          }
+          resolve();
         }
-        return (new Promise((resolve, reject) => {
-            this.connection.query(sqlStmt, values, (error, response) => {
-                if (error) {
-                    reject();
-                    throw(error.message);
-                } else {
-                    resolve(response);
-                }
-            });
-        }));
-    }
+      });
+    }));
+  }
 
-    async end() {
-        if (this.connection) {
-            return (new Promise((resolve, reject) => {
-                // connection.destroy -> bu fonksiyondan sonra hicbir sey gerceklesmez.
-                // bunun olmamasi icin connection.end kullaniyoruz.
-                this.connection.end((error) => {
-                    if (error) {
-                        reject();
-                        throw (error);
-                    } else {
-                        if (__DEBUG__) {
-                            console.log('thread : ' + this.connection.threadId + ' basariyla sonlandirildi...');
-                        }
-                        resolve();
-                    }
-                });
-            }));
+  async send_query(sqlStmt, values = []) {
+    if (!this.connection) {
+      throw new Error("Could not connect to the database.");
+    }
+    return (new Promise((resolve, reject) => {
+      this.connection.query(sqlStmt, values, (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
         }
+      });
+    }));
+  }
+
+  async end() {
+    if (this.connection) {
+      return (new Promise((resolve, reject) => {
+        this.connection.end((error) =>{
+          if (error) {
+            reject(error);
+          } else {
+            if (__DEBUG__) {
+              console.log('thread: '
+                + this.connection.threadId
+                + ' ended successfuly.');
+            }
+            this.connection = null;
+            resolve();
+          }
+        });
+      }));
     }
-}
+  }
+};
 
-const DBconnection1 = new DbConnection({multipleStatements: true});
+const DBConnection = new DbConnection({multipleStatements: true});
 
+/* if you get ER_NOT_SUPPORTED_AUTH_MODE, use IP addr in host field. */
+const host = 'localhost';
+const userName = 'root';
+const passwd = '********';
 
 (async () => {
-
-    await DBconnection1.connect('localhost', 'user', 'pass');
-    try {
-        let lastQuery = await DBconnection1.send_query('-- the query;', []);
-        if (__DEBUG__) {
-            console.log(lastQuery);
-        }
-        await DBconnection1.end();
-    
-    } catch (error) {
-        console.log(error);
+  await DBConnection.connect(host, userName, passwd);
+  try {
+    let queryResult = await DBConnection.send_query('SHOW DATABASES;', []);
+    if (__DEBUG__) {
+      console.log(queryResult);
     }
+    await DBConnection.end();
+  } catch (error) {
+    console.log(error.message);
+  }
 })();
