cb421e4b4006ca02ff5e54873a88c576fd431630
yigtuyumz <yigtuyumz@gmail.com>
2025-06-01 22:24:26 +0300

    simpler overlap detection

--- overlap.c
+++ overlap.c
@@ -1,8 +1,5 @@
 int
 does_it_overlap(void *a, void *b, unsigned long int n)
 {
-    unsigned char *p1 = (unsigned char *) a;
-    unsigned char *p2 = (unsigned char *) b;
-
-    return ((p1 < p2 + n) && (p2 < p1 + n));
+    return ((a > b) ? ((a - b) < n) : ((b - a) < n));
 }
